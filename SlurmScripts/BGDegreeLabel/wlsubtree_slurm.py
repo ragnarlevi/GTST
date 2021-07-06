8c4d86b22060a9f690cadbfcd79bd902686fac72
Ragnar Levi Gudmundarson <ragnarlevigud@gmail.com>
2021-07-06 11:27:22 +0000

    SlurmScript wl

--- SlurmScripts/BGDegreeLabel/wlsubtree_slurm.py
+++ SlurmScripts/BGDegreeLabel/wlsubtree_slurm.py
@@ -47,7 +47,7 @@ for nr_node in nr_nodes:
 
             items = ["#!/bin/bash", 
             f"#SBATCH --time=3:00:00",
-            f"#SBATCH --job-name=mmd_{tt}",
+            f"#SBATCH --job-name=wl_mmd_{tt}",
             f"#SBATCH --partition=amd-longq",
             f"#SBATCH --nodes=1",
             f"#SBATCH --ntasks-per-node=1",
