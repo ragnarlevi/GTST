49f6002f903d8cd8ff6da892f2e658e0db15f4ba
Ragnar Levi Gudmundarson <ragnarlevigud@gmail.com>
2021-07-22 12:05:42 +0000

    run scripts

--- SlurmScripts/SBMOnlyRandomLabel/run_slurm.py
+++ SlurmScripts/SBMOnlyRandomLabel/run_slurm.py
@@ -7,18 +7,119 @@ parser.add_argument('-e', '--email',metavar='', type=str, help='email')
 parser.add_argument('-B', '--NrBootstraps',metavar='', type=int, help='Give number of bootstraps')
 parser.add_argument('-N', '--NrSampleIterations',metavar='', type=int, help='Give number of sample iterations')
 parser.add_argument('-c', '--CpuPerTask',metavar='', type=int, help='cpu per task', const=4, nargs = "?")
-parser.add_argument('-i', '--wlitr',metavar='', type=int, help='Nr WL iterations')
 parser.add_argument('-norm', '--normalize',metavar='', type=int, help='Normalize kernel?')
+
+parser.add_argument('-kernel', '--kernel', type=str,metavar='', help='Kernel')
+
+parser.add_argument('-nitr', '--NumberIterations', type=int,metavar='', help='WL nr iterations')
+parser.add_argument('-wlab', '--wlab', type=int,metavar='', help='With labels?')
+
+# pyramid only
+parser.add_argument('-L', '--histogramlevel', type=int,metavar='', help='Pyramid histogram level.')
+parser.add_argument('-dim', '--dim', type=int,metavar='', help='The dimension of the hypercube.')
+
+group = parser.add_mutually_exclusive_group()
+group.add_argument('-v', '--verbose', action='store_false', help = 'print verbose')
+
+parser.add_argument('-tmax', '--tmax', type=int,metavar='', help='Maximum number of iterations.')
+parser.add_argument('-w', '--binwidth', type=float,metavar='', help='Bin width.')
+parser.add_argument('-M', '--Distance', type=str,metavar='', help='The preserved distance metric (on local sensitive hashing):')
+
+parser.add_argument('-type', '--type', type=str,metavar='', help='Type of rw, deepkernel, ....')
+parser.add_argument('-l', '--discount', type=float,metavar='', help='RW lambda')
+
+parser.add_argument('-dagh', '--DAGHeight', type=int,metavar='', help='Maximum (single) dag height. If None there is no restriction.')
+
+parser.add_argument('-sk', '--sinkhorn', type=int,metavar='', help='sinkhorn?')
 
 args = parser.parse_args()
 
 usr = args.username
 email = args.email
 cpu_per_task = args.CpuPerTask
-wl_it = args.wlitr
+
 norm = args.normalize
 B = args.NrBootstraps
 N = args.NrSampleIterations
+
+
+kernel_name = args.kernel
+# add parameters parsed, may be none
+ksp = dict()
+
+# WL iterations
+ksp['nitr'] = args.NumberIterations   
+
+# with labels?
+if args.wlab is None:
+    ksp['with_labels'] = bool(1)
+else:
+    ksp['with_labels'] = bool(args.wlab)
+
+ksp['L'] = args.histogramlevel
+ksp['dim'] = args.dim
+
+ksp['w'] = args.binwidth
+ksp['tmax'] = args.tmax
+ksp['M'] = args.Distance
+
+ksp['type'] = args.type  
+ksp['discount'] = args.discount   
+
+ksp['dagh'] = args.DAGHeight
+ksp['sinkhorn'] = bool(args.sinkhorn)
+
+
+if kernel_name == 'wl':
+    k_val = 'WLsubtree'
+    unique_identifier = f'nitr_{ksp["nitr"]}'
+    script_args = f'-nitr {ksp["nitr"]}'
+elif kernel_name == 'sp':
+    k_val = 'SP'
+    unique_identifier = f'wlab_{ksp["wlab"]}'
+    script_args = f'-wlab {ksp["wlab"]}'
+elif kernel_name == 'pyramid':
+    k_val = 'PYRAMID'
+    unique_identifier = f'wlab{ksp["wlab"]}_L_{ksp["L"]}_dim_{ksp["dim"]}'
+    script_args = f'-wlab {ksp["wlab"]} -L {ksp["L"]} -dim {ksp["dim"]}'
+elif kernel_name == 'prop':
+    k_val = 'PROP'
+    unique_identifier = f'w{ksp["w"]}_tmax{ksp["tmax"]}_M{ksp["M"]}'
+    script_args = f'-w {ksp["w"]} -tmax {ksp["tmax"]} -M {ksp["M"]}'
+elif kernel_name == 'wloa':
+    k_val = 'WLOA'
+    unique_identifier = f'nitr_{ksp["nitr"]}_'
+    script_args = f'-nitr {ksp["nitr"]}'
+elif kernel_name == 'vh':
+    # vertex histogram
+    k_val = 'VH'
+    unique_identifier = f'_'
+    script_args = f''
+elif kernel_name == 'rw':
+    k_val = 'RW'
+    unique_identifier = f'rw_{ksp["type"]}_l_{ksp["discount"]}_wlab_{ksp["wlab"]}'
+    script_args = f'-type {ksp["type"]} -l {ksp["discount"]} -wlab {ksp["wlab"]}'
+elif kernel_name == 'odd':
+    k_val = 'ODD'
+    unique_identifier = f'dagh_{ksp["dagh"]}'
+    script_args = f'-dagh {ksp["dagh"]} '
+elif kernel_name == 'dk':
+    k_val = 'DK'
+    unique_identifier = f'wl_{ksp["nitr"]}_t_{ksp["type"]}'
+    script_args = f'-nitr {ksp["nitr"]} -type {ksp["type"]}'
+elif kernel_name == 'wwl':
+    k_val = 'WWL'
+    unique_identifier = f'wl_{ksp["nitr"]}_l_{ksp["discount"]}_sink_{ksp["sinkhorn"]}'
+    script_args = f'-nitr {ksp["nitr"]} -l {ksp["discount"]} -sk {ksp["sinkhorn"]}'
+else:
+    raise ValueError(f'No kernel names {kernel_name}')
+
+
+from pathlib import Path
+# Create directories if they do not exist
+Path(f"data/SBMOnlyRandomLabel/{k_val}").mkdir(parents=True, exist_ok=True)
+Path(f"/home/{usr}/projects/MMDGraph/SlurmBatch/SBMOnlyRandomLabel/{k_val}").mkdir(parents=True, exist_ok=True)
+
 
 path = f"/home/{usr}/projects/MMDGraph/SlurmBatch/SBMOnlyRandomLabel"
 
@@ -32,19 +133,19 @@ for nr_sample in nr_samples:
 
                     
         # Note that in the slurm batch file we set another working directory which is the reason for this data_name path
-        data_name = f'data/SBMOnlyRandomLabel/WLsubtree/wl_n_{nr_sample}_noise_{noise}_wl_{wl_it}_norm_{norm}.pkl'
+        data_name = f'data/SBMOnlyRandomLabel/{k_val}/n_{nr_sample}_noise_{noise}_norm_{norm}_{unique_identifier}.pkl'
         
-        job_file = path + f"/WLsubtree/wl_n_{nr_sample}_noise_{noise}_wl_{wl_it}_norm_{norm}.slurm"
+        job_file = path + f"/{k_val}/n_{nr_sample}_noise_{noise}_norm_{norm}_{unique_identifier}.slurm"
 
         items = ["#!/bin/bash", 
         f"#SBATCH --time=8:00:00",
-        f"#SBATCH --job-name=wl_sbmORL_n_{nr_sample}_noise_{noise}_wl_{wl_it}_norm_{norm}",
+        f"#SBATCH --job-name={k_val}_sbmORL_n_{nr_sample}_noise_{noise}_norm_{norm}_{unique_identifier}",
         f"#SBATCH --partition=amd-longq",
         f"#SBATCH --nodes=1",
         f"#SBATCH --ntasks-per-node=1",
         f"#SBATCH --cpus-per-task={cpu_per_task}",
-        f"#SBATCH --output=/home/{usr}/projects/MMDGraph/outputs/sbmorl_n_{nr_sample}_noise_{noise}_k_wl_{wl_it}_norm_{norm}.out",
-        f"#SBATCH --error=/home/{usr}/projects/MMDGraph/errors/sbmorl_n_{nr_sample}_noise_{noise}_k_wl_{wl_it}_norm_{norm}.err",
+        f"#SBATCH --output=/home/{usr}/projects/MMDGraph/outputs/{k_val}_sbmorl_n_{nr_sample}_noise_{noise}_norm_{norm}_{unique_identifier}.out",
+        f"#SBATCH --error=/home/{usr}/projects/MMDGraph/errors/{k_val}_sbmorl_n_{nr_sample}_noise_{noise}_norm_{norm}_{unique_identifier}.err",
         f"#SBATCH --mail-user={email}",
         f"#SBATCH --mail-type=FAIL",
         "module purge",
@@ -54,7 +155,7 @@ for nr_sample in nr_samples:
         ]
         
 
-        items.append(f"python3 Experiments/SBMOnlyRandomLabel/wl_subtree.py -B {B} -N {B} -n1 {nr_sample} -n2 {nr_sample} -p {data_name} -norm {norm} -nitr {wl_it} -d {cpu_per_task} -noise {noise}")
+        items.append(f"python3 Experiments/SBMOnlyRandomLabel/run.py -B {B} -N {B} -n1 {nr_sample} -n2 {nr_sample} -p {data_name} -norm {norm} -d {cpu_per_task} -noise {noise} {script_args}")
 
 
 
