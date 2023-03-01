6ef7e66a4ed35bac5315a953fda2702140f98d87
Ragnar <ragnarlevigud@gmail.com>
2023-03-01 23:23:29 +0000

    renaming

---
+++ GTST/__init__.py
@@ -0,0 +1,6 @@
+from GTST.glasso import glasso_wrapper
+from GTST.MMD import MMD
+
+from GTST.kernels import RandomWalk
+from GTST.kernels import GNTK
+from GTST.kernels import WWL
--- MMDGraph/__init__.py
+++
@@ -1,6 +0,0 @@
-from MMDGraph.glasso import glasso_wrapper
-from MMDGraph.MMD import MMD
-
-from MMDGraph.kernels import RandomWalk
-from MMDGraph.kernels import GNTK
-from MMDGraph.kernels import WWL
