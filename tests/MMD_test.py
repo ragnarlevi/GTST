134953160ac82dc02ff236da92c8e42ad9f003f7
Ragnar <ragnarlevigud@gmail.com>
2023-04-19 14:17:08 +0000

    All tests included

--- tests/MMD_test.py
+++ tests/MMD_test.py
@@ -8,120 +8,120 @@ from tests.utils import generate_H0_false, assert_low_p_val,  generate_H0_false_
 
 
 
-# def test_MMD():
+def test_MMD():
 
-#     # Test various kernels, similar to kernels_test
+    # Test various kernels, similar to kernels_test
 
-#     # Test Random Walk
-#     g1, g2 = generate_H0_false()
-#     MMD_out = MMD()
-#     MMD_out.fit(G1 = g1, G2 = g2, kernel = 'RW_ARKU_plus', mmd_estimators = ['MMD_u', 'MMD_b', 'MMD_l', 'MONK_EST'], r = 4, c = 0.001, Q = 5, verbose = True)
-#     assert_low_p_val(MMD_out.p_values)
+    # Test Random Walk
+    g1, g2 = generate_H0_false()
+    MMD_out = MMD()
+    MMD_out.fit(G1 = g1, G2 = g2, kernel = 'RW_ARKU_plus', mmd_estimators = ['MMD_u', 'MMD_b', 'MMD_l', 'MONK_EST'], r = 6, c = 0.001, Q = 5, verbose = True)
+    assert_low_p_val(MMD_out.p_values)
 
-#     g1, g2 = generate_H0_false_but_same_topology()
-#     MMD_out = MMD()
-#     MMD_out.fit(G1 = g1, G2 = g2, kernel = 'RW_ARKU_plus', mmd_estimators = ['MMD_u', 'MMD_b', 'MMD_l'], r = 3, c = 1e-4, verbose = True, node_attr = 'attr',
-#                 bootstrap_method = 'BootstrapScheme', check_symmetry = True)
-#     assert_low_p_val(MMD_out.p_values)
+    g1, g2 = generate_H0_false_but_same_topology()
+    MMD_out = MMD()
+    MMD_out.fit(G1 = g1, G2 = g2, kernel = 'RW_ARKU_plus', mmd_estimators = ['MMD_u', 'MMD_b', 'MMD_l'], r = 6, c = 1e-4, verbose = True, node_attr = 'attr',
+                bootstrap_method = 'BootstrapScheme', check_symmetry = True)
+    assert_low_p_val(MMD_out.p_values)
 
-#     g1, g2 = generate_H0_false_but_same_topology()
-#     MMD_out = MMD()
-#     MMD_out.fit(G1 = g1, G2 = g2, kernel = 'RW_ARKU_edge', mmd_estimators = ['MMD_u', 'MMD_b', 'MMD_l'], r = 3, c = 1e-4, verbose = True, 
-#                 edge_label = 'edge_label', unique_edge_labels = ['a', 'b'], check_symmetry = True)
-#     assert_low_p_val(MMD_out.p_values)
+    g1, g2 = generate_H0_false_but_same_topology()
+    MMD_out = MMD()
+    MMD_out.fit(G1 = g1, G2 = g2, kernel = 'RW_ARKU_edge', mmd_estimators = ['MMD_u', 'MMD_b', 'MMD_l'], r = 6, c = 1e-4, verbose = True, 
+                edge_label = 'edge_label', unique_edge_labels = ['a', 'b'], check_symmetry = True)
+    assert_low_p_val(MMD_out.p_values)
 
-#     g1, g2 = generate_H0_false_but_same_topology()
-#     MMD_out = MMD()
-#     MMD_out.fit(G1 = g1, G2 = g2, kernel = 'RW_ARKL', mmd_estimators = ['MMD_u', 'MMD_b', 'MMD_l'], r = 3, c = 1e-4, verbose = True, 
-#                 node_label = 'label', unique_node_labels = ['a', 'b', 'c'])
-#     assert_low_p_val(MMD_out.p_values)
+    g1, g2 = generate_H0_false_but_same_topology()
+    MMD_out = MMD()
+    MMD_out.fit(G1 = g1, G2 = g2, kernel = 'RW_ARKL', mmd_estimators = ['MMD_u', 'MMD_b', 'MMD_l'], r = 6, c = 1e-4, verbose = True, 
+                node_label = 'label', unique_node_labels = ['a', 'b', 'c'])
+    assert_low_p_val(MMD_out.p_values)
 
-#     g1, g2 = generate_H0_false_directed()
-#     MMD_out = MMD()
-#     MMD_out.fit(G1 = g1, G2 = g2, kernel = 'RW_ARKU', mmd_estimators = ['MMD_u', 'MMD_b', 'MMD_l'], r = 3, c = 1e-4, verbose = True, edge_attr = 'weight')
-#     assert_low_p_val(MMD_out.p_values)
+    g1, g2 = generate_H0_false_directed()
+    MMD_out = MMD()
+    MMD_out.fit(G1 = g1, G2 = g2, kernel = 'RW_ARKU', mmd_estimators = ['MMD_u', 'MMD_b', 'MMD_l'], r = 6, c = 1e-4, verbose = True, edge_attr = 'weight')
+    assert_low_p_val(MMD_out.p_values)
 
-#     # Test GNTK
-#     g1, g2 = generate_H0_false()
-#     MMD_out = MMD()
-#     MMD_out.fit(G1 = g1, G2 = g2, kernel = 'GNTK', mmd_estimators = 'MMD_u', num_layers = 2, num_mlp_lauers = 2, jk = True, scale = 'uniform', degree_as_tag = True, verbose =True)
-#     assert_low_p_val(MMD_out.p_values)
-#     g1, g2 = generate_H0_false_but_same_topology()
-#     MMD_out = MMD()
-#     MMD_out.fit(G1 = g1, G2 = g2, kernel = 'GNTK', mmd_estimators = 'MMD_u', num_layers = 2, num_mlp_lauers = 2, jk = False, scale = 'degree', node_attr = 'attr', verbose =True)
-#     assert_low_p_val(MMD_out.p_values)
+    # Test GNTK
+    g1, g2 = generate_H0_false()
+    MMD_out = MMD()
+    MMD_out.fit(G1 = g1, G2 = g2, kernel = 'GNTK', mmd_estimators = 'MMD_u', num_layers = 2, num_mlp_lauers = 2, jk = True, scale = 'uniform', degree_as_tag = True, verbose =True)
+    assert_low_p_val(MMD_out.p_values)
+    g1, g2 = generate_H0_false_but_same_topology()
+    MMD_out = MMD()
+    MMD_out.fit(G1 = g1, G2 = g2, kernel = 'GNTK', mmd_estimators = 'MMD_u', num_layers = 2, num_mlp_lauers = 2, jk = False, scale = 'degree', node_attr = 'attr', verbose =True)
+    assert_low_p_val(MMD_out.p_values)
 
-#     # Test DK
-#     g1, g2 = generate_H0_false_but_same_topology()
-#     MMD_out = MMD()
-#     MMD_out.fit(G1 = g1, G2 = g2, kernel = 'DK', mmd_estimators = 'MMD_u', type = 'wl', wl_it = 4, node_label = 'label')
-#     assert_low_p_val(MMD_out.p_values)
+    # Test DK
+    g1, g2 = generate_H0_false_but_same_topology()
+    MMD_out = MMD()
+    MMD_out.fit(G1 = g1, G2 = g2, kernel = 'DK', mmd_estimators = 'MMD_u', type = 'wl', wl_it = 4, node_label = 'label')
+    assert_low_p_val(MMD_out.p_values)
     
-#     MMD_out = MMD()
-#     MMD_out.fit(G1 = g1, G2 = g2, kernel = 'DK', mmd_estimators = 'MMD_u', type = 'sp', node_label = 'label')
-#     assert_low_p_val(MMD_out.p_values)
+    MMD_out = MMD()
+    MMD_out.fit(G1 = g1, G2 = g2, kernel = 'DK', mmd_estimators = 'MMD_u', type = 'sp', node_label = 'label')
+    assert_low_p_val(MMD_out.p_values)
 
 
-#     # Test WWL
-#     MMD_out = MMD()
-#     MMD_out.fit(G1 = g1, G2 = g2, kernel = 'WWL', mmd_estimators = 'MMD_u', discount = 0.1, h = 2, node_label = 'label')
-#     assert_low_p_val(MMD_out.p_values)
+    # Test WWL
+    MMD_out = MMD()
+    MMD_out.fit(G1 = g1, G2 = g2, kernel = 'WWL', mmd_estimators = 'MMD_u', discount = 0.1, h = 2, node_label = 'label')
+    assert_low_p_val(MMD_out.p_values)
 
-#     # Test one Grakel kernel
-#     kernel = [{"name": "weisfeiler_lehman", "n_iter": 1}, {"name": "vertex_histogram"}]
-#     MMD_out = MMD()
-#     MMD_out.fit(G1 = g1, G2 = g2, kernel = kernel, mmd_estimators = 'MMD_u', node_label = 'label')
-#     assert_low_p_val(MMD_out.p_values)
-
-
-
-#     # Test graph fitting
-
-#     X1,X2 = generate_Xs_H0_false()
-#     MMD_out = MMD()
-#     MMD_out.estimate_graphs(X1,X2,window_size=400, alpha = np.exp(np.linspace(-5,-2,100)),beta = 0.5, nonparanormal=False,scale = False)
-#     MMD_out.fit( kernel = 'RW_ARKU_plus', mmd_estimators = 'MMD_u', r = 3, c = 0.1, edge_attr = 'weight')
-#     assert_low_p_val(MMD_out.p_values)
-
-#     MMD_out = MMD()
-#     MMD_out.estimate_graphs(X1,X2,window_size=400, alpha = np.exp(np.linspace(-5,-2,100)),beta = 0.5, nonparanormal=True,scale = False)
-#     MMD_out.fit( kernel = 'RW_ARKU_plus', mmd_estimators = 'MMD_u', r = 3, c = 0.1, edge_attr = 'weight')
-#     assert_low_p_val(MMD_out.p_values)
-
-#     MMD_out = MMD()
-#     MMD_out.estimate_graphs(X1,X2,window_size=400, alpha = np.exp(np.linspace(-5,-2,100)),beta = 0.5, nonparanormal=False,scale = True)
-#     MMD_out.fit( kernel = 'RW_ARKU_plus', mmd_estimators = 'MMD_u', r = 3, c = 0.1, edge_attr = 'weight')
-#     assert_low_p_val(MMD_out.p_values)
+    # Test one Grakel kernel
+    kernel = [{"name": "weisfeiler_lehman", "n_iter": 1}, {"name": "vertex_histogram"}]
+    MMD_out = MMD()
+    MMD_out.fit(G1 = g1, G2 = g2, kernel = kernel, mmd_estimators = 'MMD_u', node_label = 'label')
+    assert_low_p_val(MMD_out.p_values)
 
 
-#     label_dict = {'1':{j:i for j,i in enumerate(['a']*8 + ['b']*3)}, 
-#               '2':{j:i for j,i in enumerate(['a']*4 + ['b']*7)}}
-#     kernel = [{"name": "weisfeiler_lehman", "n_iter": 2}, {"name": "vertex_histogram"}]
-#     MMD_out = MMD()
-#     MMD_out.estimate_graphs(X1,X2,window_size=400, alpha = np.exp(np.linspace(-5,-2,100)),beta = 0.5, nonparanormal=False,scale = False, set_labels= label_dict)
-#     MMD_out.fit(kernel = kernel, mmd_estimators = 'MMD_u', node_label = 'label')
-#     assert_low_p_val(MMD_out.p_values)
-#     assert_low_p_val(MMD_out.p_values, "label_dict")
+
+    # Test graph fitting
+
+    X1,X2 = generate_Xs_H0_false()
+    MMD_out = MMD()
+    MMD_out.estimate_graphs(X1,X2,window_size=400, alpha = np.exp(np.linspace(-5,-2,100)),beta = 0.5, nonparanormal=False,scale = False)
+    MMD_out.fit( kernel = 'RW_ARKU_plus', mmd_estimators = 'MMD_u', r = 3, c = 0.1, edge_attr = 'weight')
+    assert_low_p_val(MMD_out.p_values)
+
+    MMD_out = MMD()
+    MMD_out.estimate_graphs(X1,X2,window_size=400, alpha = np.exp(np.linspace(-5,-2,100)),beta = 0.5, nonparanormal=True,scale = False)
+    MMD_out.fit( kernel = 'RW_ARKU_plus', mmd_estimators = 'MMD_u', r = 3, c = 0.1, edge_attr = 'weight')
+    assert_low_p_val(MMD_out.p_values)
+
+    MMD_out = MMD()
+    MMD_out.estimate_graphs(X1,X2,window_size=400, alpha = np.exp(np.linspace(-5,-2,100)),beta = 0.5, nonparanormal=False,scale = True)
+    MMD_out.fit( kernel = 'RW_ARKU_plus', mmd_estimators = 'MMD_u', r = 3, c = 0.1, edge_attr = 'weight')
+    assert_low_p_val(MMD_out.p_values)
 
 
-#     def label_function(X):
-#         m = np.mean(X,axis = 0)
-#         return {i:str(np.round(m[i],1)) for i in range(len(m))}
+    label_dict = {'1':{j:i for j,i in enumerate(['a']*8 + ['b']*3)}, 
+              '2':{j:i for j,i in enumerate(['a']*4 + ['b']*7)}}
+    kernel = [{"name": "weisfeiler_lehman", "n_iter": 2}, {"name": "vertex_histogram"}]
+    MMD_out = MMD()
+    MMD_out.estimate_graphs(X1,X2,window_size=400, alpha = np.exp(np.linspace(-5,-2,100)),beta = 0.5, nonparanormal=False,scale = False, set_labels= label_dict)
+    MMD_out.fit(kernel = kernel, mmd_estimators = 'MMD_u', node_label = 'label')
+    assert_low_p_val(MMD_out.p_values)
+    assert_low_p_val(MMD_out.p_values, "label_dict")
 
-#     kernel = [{"name": "weisfeiler_lehman", "n_iter": 2}, {"name": "vertex_histogram"}]
-#     MMD_out = MMD()
-#     MMD_out.estimate_graphs(X1,X2,window_size=400, alpha = np.exp(np.linspace(-5,-2,100)),beta = 0.5, nonparanormal=False,scale = False, set_labels= label_function)
-#     MMD_out.fit(kernel = kernel, mmd_estimators = 'MMD_u', node_label = 'label')
-#     assert_low_p_val(MMD_out.p_values, "label_function")
+
+    def label_function(X):
+        m = np.mean(X,axis = 0)
+        return {i:str(np.round(m[i],1)) for i in range(len(m))}
+
+    kernel = [{"name": "weisfeiler_lehman", "n_iter": 2}, {"name": "vertex_histogram"}]
+    MMD_out = MMD()
+    MMD_out.estimate_graphs(X1,X2,window_size=400, alpha = np.exp(np.linspace(-5,-2,100)),beta = 0.5, nonparanormal=False,scale = False, set_labels= label_function)
+    MMD_out.fit(kernel = kernel, mmd_estimators = 'MMD_u', node_label = 'label')
+    assert_low_p_val(MMD_out.p_values, "label_function")
 
 
-#     def attr_function(X):
-#         return np.expand_dims(np.mean(X,axis = 0),axis=1)
+    def attr_function(X):
+        return np.expand_dims(np.mean(X,axis = 0),axis=1)
 
-#     MMD_out = MMD()
-#     MMD_out.estimate_graphs(X1,X2,window_size=400, alpha = np.exp(np.linspace(-5,-2,100)),beta = 0.5, nonparanormal=False,scale = False, set_attributes = attr_function)
-#     MMD_out.fit(kernel = 'GNTK', mmd_estimators = 'MMD_u', num_layers = 2, num_mlp_lauers = 2, jk = False, scale = 'degree', node_attr = 'attr', verbose =True)
-#     assert_low_p_val(MMD_out.p_values, "attr_function")
+    MMD_out = MMD()
+    MMD_out.estimate_graphs(X1,X2,window_size=400, alpha = np.exp(np.linspace(-5,-2,100)),beta = 0.5, nonparanormal=False,scale = False, set_attributes = attr_function)
+    MMD_out.fit(kernel = 'GNTK', mmd_estimators = 'MMD_u', num_layers = 2, num_mlp_lauers = 2, jk = False, scale = 'degree', node_attr = 'attr', verbose =True)
+    assert_low_p_val(MMD_out.p_values, "attr_function")
 
 
 
--- tests/kernels_test.py
+++ tests/kernels_test.py
@@ -7,26 +7,26 @@ from tests.utils import generate_H0_false,assert_positive_eig,  generate_H0_fals
 
 def test_RandomWalk_ARKU_plus():
     g1, g2 = generate_H0_false()
-    rw = RandomWalk(g1 + g2,r = 3, c = 1e-5)
+    rw = RandomWalk(g1 + g2,r = 6, c = 1e-5)
     rw.fit('ARKU_plus', verbose=True)
     assert_positive_eig(rw.K)
 
-    # # Test MMD, should give very low p-val
-    # pval = BoostrapMethods(list_of_functions=[MMD_u, MMD_b, MMD_l, MONK_EST], function_arguments={'MMD_u':{'n1':len(g1), 'n2':len(g2)}, 
-    #                                                                                     'MMD_b':{'n1':len(g1), 'n2':len(g2)}, 
-    #                                                                                     'MMD_l':{'n1':len(g1), 'n2':len(g2)}, 
-    #                                                                                     'MONK_EST':{'n1':len(g1), 'n2':len(g2), 'Q':5}})
-    # pval.Bootstrap(rw.K, 1000)
-    # assert np.all(np.fromiter(pval.p_values.values(), dtype=float) < 0.01), f"Some p value too high {pval.p_values}"
+    # Test MMD, should give very low p-val
+    pval = BoostrapMethods(list_of_functions=[MMD_u, MMD_b, MMD_l, MONK_EST], function_arguments={'MMD_u':{'n1':len(g1), 'n2':len(g2)}, 
+                                                                                        'MMD_b':{'n1':len(g1), 'n2':len(g2)}, 
+                                                                                        'MMD_l':{'n1':len(g1), 'n2':len(g2)}, 
+                                                                                        'MONK_EST':{'n1':len(g1), 'n2':len(g2), 'Q':5}})
+    pval.Bootstrap(rw.K, 1000)
+    assert np.all(np.fromiter(pval.p_values.values(), dtype=float) < 0.01), f"Some p value too high {pval.p_values}"
 
     
     g1, g2 = generate_H0_false_but_same_topology()
-    rw = RandomWalk(g1 + g2,r = 3, c = 1e-5,edge_attr='weight')
+    rw = RandomWalk(g1 + g2,r = 6, c = 1e-5,edge_attr='weight')
     rw.fit('ARKU_plus', verbose=True)
     assert_positive_eig(rw.K)
     assert_low_p_val_from_K(rw.K, g1,g2)
 
-    rw = RandomWalk(g1 + g2,r = 3, c = 1e-8,node_attr='attr')
+    rw = RandomWalk(g1 + g2,r = 6, c = 1e-8,node_attr='attr')
     rw.fit('ARKU_plus', verbose=True)
     assert_positive_eig(rw.K)
     assert_low_p_val_from_K(rw.K, g1,g2)
@@ -34,98 +34,98 @@ def test_RandomWalk_ARKU_plus():
 
 
 
-# def test_RandomWalk_ARKL():
-#     g1, g2 = generate_H0_false()
-#     unique_node_labels= set(np.concatenate([list(nx.get_node_attributes(g, 'label').values()) for g in g1+g2]))
-#     rw = RandomWalk(g1 + g2, r = 3, c = 0.001, node_label='label', unique_node_labels = unique_node_labels)
-#     rw.fit('ARKL', verbose=True)
-#     assert_positive_eig(rw.K)
-#     assert_low_p_val_from_K(rw.K, g1,g2)
+def test_RandomWalk_ARKL():
+    g1, g2 = generate_H0_false()
+    unique_node_labels= set(np.concatenate([list(nx.get_node_attributes(g, 'label').values()) for g in g1+g2]))
+    rw = RandomWalk(g1 + g2, r = 6, c = 0.001, node_label='label', unique_node_labels = unique_node_labels)
+    rw.fit('ARKL', verbose=True)
+    assert_positive_eig(rw.K)
+    assert_low_p_val_from_K(rw.K, g1,g2)
 
-#     g1, g2 = generate_H0_false_but_same_topology()
-#     unique_node_labels= ['a', 'b', 'c']
-#     rw = RandomWalk(g1 + g2, r = 3, c = 0.001, node_label='label', unique_node_labels = unique_node_labels)
-#     rw.fit('ARKL', verbose=True)
-#     assert_positive_eig(rw.K)
-#     assert_low_p_val_from_K(rw.K, g1,g2)
-
-
-# def test_RandomWalk_ARKU():
-#     g1, g2 = generate_H0_false()
-#     rw = RandomWalk(g1 + g2, r = 3, c = 0.001)
-#     rw.fit('ARKU',verbose=True)
-#     assert_positive_eig(rw.K)
-#     assert_low_p_val_from_K(rw.K, g1,g2)
-#     # Directed
-#     g1, g2 =  generate_H0_false_directed()
-#     rw = RandomWalk(g1 + g2, r = 3, c = 0.001)
-#     rw.fit('ARKU',verbose=True)
-#     assert_positive_eig(rw.K)
-#     assert_low_p_val_from_K(rw.K, g1,g2)
+    g1, g2 = generate_H0_false_but_same_topology()
+    unique_node_labels= ['a', 'b', 'c']
+    rw = RandomWalk(g1 + g2, r = 6, c = 0.001, node_label='label', unique_node_labels = unique_node_labels)
+    rw.fit('ARKL', verbose=True)
+    assert_positive_eig(rw.K)
+    assert_low_p_val_from_K(rw.K, g1,g2)
 
 
-# def test_RandomWalk_ARKU_edge():
-#     g1, g2 = generate_H0_false_but_same_topology()
-#     rw = RandomWalk(g1 + g2, r = 3, c = 0.0001, edge_label='edge_label',unique_edge_labels= ['a', 'b'])
-#     rw.fit('ARKU_edge', verbose=True)
-#     assert_positive_eig(rw.K)
-#     assert_low_p_val_from_K(rw.K, g1,g2)
+def test_RandomWalk_ARKU():
+    g1, g2 = generate_H0_false()
+    rw = RandomWalk(g1 + g2, r = 6, c = 0.001)
+    rw.fit('ARKU',verbose=True)
+    assert_positive_eig(rw.K)
+    assert_low_p_val_from_K(rw.K, g1,g2)
+    # Directed
+    g1, g2 =  generate_H0_false_directed()
+    rw = RandomWalk(g1 + g2, r = 6, c = 0.001)
+    rw.fit('ARKU',verbose=True)
+    assert_positive_eig(rw.K)
+    assert_low_p_val_from_K(rw.K, g1,g2)
 
 
-# def test_GNTK():
-#     g1, g2 = generate_H0_false()
-
-#     gntk_kernel = GNTK(num_layers= 2, num_mlp_layers = 2, 
-#                                 jk = True, scale = 'uniform', normalize=1)
-#     gntk_kernel.fit_all(g1 + g2,degree_as_tag=True,features= None)
-#     assert_positive_eig(gntk_kernel.K)
-#     assert_low_p_val_from_K(gntk_kernel.K, g1,g2)
-
-#     gntk_kernel = GNTK(num_layers= 2, num_mlp_layers = 2, 
-#                             jk = False, scale = 'uniform', normalize=1)
-#     gntk_kernel.fit_all(g1 + g2,degree_as_tag=True,features= None)
-#     assert_positive_eig(gntk_kernel.K)
-#     assert_low_p_val_from_K(gntk_kernel.K, g1,g2)
-
-#     # Same graphs but different attributes
-#     g1, g2 = generate_H0_false_but_same_topology()
-#     gntk_kernel = GNTK(num_layers= 2, num_mlp_layers = 2, 
-#                             jk = False, scale = 'degree', normalize=1)
-#     gntk_kernel.fit_all(g1 + g2,degree_as_tag=False,features= 'attr')
-#     assert_positive_eig(gntk_kernel.K)
-#     assert_low_p_val_from_K(gntk_kernel.K, g1,g2)
+def test_RandomWalk_ARKU_edge():
+    g1, g2 = generate_H0_false_but_same_topology()
+    rw = RandomWalk(g1 + g2, r = 6, c = 0.0001, edge_label='edge_label',unique_edge_labels= ['a', 'b'])
+    rw.fit('ARKU_edge', verbose=True)
+    assert_positive_eig(rw.K)
+    assert_low_p_val_from_K(rw.K, g1,g2)
 
 
-# def test_DK():
-#     g1, g2 = generate_H0_false_but_same_topology()
-#     dk_kernel = DK(type = 'wl', wl_it = 2, opt_type = None, nodel_label='label')
-#     dk_kernel.fit_transform(g1+g2)
-#     assert_positive_eig(dk_kernel.K)
-#     assert_low_p_val_from_K(dk_kernel.K, g1,g2)
+def test_GNTK():
+    g1, g2 = generate_H0_false()
+
+    gntk_kernel = GNTK(num_layers= 2, num_mlp_layers = 2, 
+                                jk = True, scale = 'uniform', normalize=1)
+    gntk_kernel.fit_all(g1 + g2,degree_as_tag=True,features= None)
+    assert_positive_eig(gntk_kernel.K)
+    assert_low_p_val_from_K(gntk_kernel.K, g1,g2)
+
+    gntk_kernel = GNTK(num_layers= 2, num_mlp_layers = 2, 
+                            jk = False, scale = 'uniform', normalize=1)
+    gntk_kernel.fit_all(g1 + g2,degree_as_tag=True,features= None)
+    assert_positive_eig(gntk_kernel.K)
+    assert_low_p_val_from_K(gntk_kernel.K, g1,g2)
+
+    # Same graphs but different attributes
+    g1, g2 = generate_H0_false_but_same_topology()
+    gntk_kernel = GNTK(num_layers= 2, num_mlp_layers = 2, 
+                            jk = False, scale = 'degree', normalize=1)
+    gntk_kernel.fit_all(g1 + g2,degree_as_tag=False,features= 'attr')
+    assert_positive_eig(gntk_kernel.K)
+    assert_low_p_val_from_K(gntk_kernel.K, g1,g2)
 
 
-#     dk_kernel = DK(type = 'sp', opt_type = None,
-#                             vector_size= 2, window=2, min_count=0, 
-#                             normalize = 0, nodel_label='label')
-#     dk_kernel.fit_transform(g1+g2)
-#     assert_positive_eig(dk_kernel.K)
-#     assert_low_p_val_from_K(dk_kernel.K, g1,g2)
-
-#     dk_kernel = DK(type = 'sp', opt_type = 'word2vec',
-#                         vector_size= 2, window=2, min_count=0, 
-#                         normalize = 0, nodel_label='label')
-#     dk_kernel.fit_transform(g1+g2)
-#     assert_positive_eig(dk_kernel.K)
-#     assert_low_p_val_from_K(dk_kernel.K, g1,g2)
+def test_DK():
+    g1, g2 = generate_H0_false_but_same_topology()
+    dk_kernel = DK(type = 'wl', wl_it = 2, opt_type = None, nodel_label='label')
+    dk_kernel.fit_transform(g1+g2)
+    assert_positive_eig(dk_kernel.K)
+    assert_low_p_val_from_K(dk_kernel.K, g1,g2)
 
 
-# def test_WWL():
-#     g1, g2 = generate_H0_false_but_same_topology()
+    dk_kernel = DK(type = 'sp', opt_type = None,
+                            vector_size= 2, window=2, min_count=0, 
+                            normalize = 0, nodel_label='label')
+    dk_kernel.fit_transform(g1+g2)
+    assert_positive_eig(dk_kernel.K)
+    assert_low_p_val_from_K(dk_kernel.K, g1,g2)
 
-#     wwl_kernel = WWL(param =dict(discount=0.1, h=2, sinkhorn=False, label_name='label'))
-#     wwl_kernel.fit_transform(g1+g2)
-#     assert_positive_eig(wwl_kernel.K)
-#     assert_low_p_val_from_K(wwl_kernel.K, g1,g2)
+    dk_kernel = DK(type = 'sp', opt_type = 'word2vec',
+                        vector_size= 2, window=2, min_count=0, 
+                        normalize = 0, nodel_label='label')
+    dk_kernel.fit_transform(g1+g2)
+    assert_positive_eig(dk_kernel.K)
+    assert_low_p_val_from_K(dk_kernel.K, g1,g2)
+
+
+def test_WWL():
+    g1, g2 = generate_H0_false_but_same_topology()
+
+    wwl_kernel = WWL(param =dict(discount=0.1, h=2, sinkhorn=False, label_name='label'))
+    wwl_kernel.fit_transform(g1+g2)
+    assert_positive_eig(wwl_kernel.K)
+    assert_low_p_val_from_K(wwl_kernel.K, g1,g2)
 
 
     
